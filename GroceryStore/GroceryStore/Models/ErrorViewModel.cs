227b500246c8a7f03c2ba843f5b7b22a85f382bc
Vanina Nikolova <67506297+nikolova18@users.noreply.github.com>
2022-01-14 14:57:27 +0200

    Initial Creating

---
+++ GroceryStore/GroceryStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace GroceryStore.Models
+{
+    using System;
+
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
